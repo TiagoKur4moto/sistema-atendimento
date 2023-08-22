df2b296cf4141992ff4f9fc117cf50a81034dc24
Tiago Kuramoto <tiago.kuramoto@estudante.ifms.edu.br>
2023-08-21 22:43:21 -0400

    add funcoes e Eventos

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,24 @@ const inputNome =
  document.querySelector('#input-nome')
  const selectPrioridade =
  document.querySelector('#select-prioridade')
-const btnIncluir = document.querySelector('#btn-incluir')
-const ulLista = document.querySelector('#ul-lista')
-const displayNome = document.querySelector('#display-nome')
-const btnChamar = document.querySelector('#btn-chamar')
+const btnIncluir = 
+document.querySelector('#btn-incluir')
+const ulLista = 
+document.querySelector('#ul-lista')
+const displayNome = 
+document.querySelector('#display-nome')
+const btnChamar = 
+document.querySelector('#btn-chamar')
 const listaDeAtendimento = new Array()
+
+function incluir(){
+    listaDeAtendimento.push(inputNome.value)
+    listar()
+}
+function listar(){
+    for(let nome of listaDeAtendimento){
+        console.log(nome)
+    }
+}
+//eventos
+btnIncluir.onclick = incluir
